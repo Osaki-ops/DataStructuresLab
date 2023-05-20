df3d113add8709d282807c0abb254c22cbb4d847
Aryan Sheregar <72085890+Osaki-ops@users.noreply.github.com>
2023-05-20 13:07:07 +0530

    Update stack_adt.c
    Fixed the errors with push_stack()

--- stack_adt.c
+++ stack_adt.c
@@ -1,98 +1,86 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-struct node
-{
-    void* data;
-    struct node* link;
-
-};
-
-struct stack
-{
+struct stack {
     int count;
     struct node* top;
 };
 
-struct stack* create_stack(void)//creates the stack counter and top pointer using malloc.
-{
+struct node {
+    void* data;
+    struct node* link;
+};
+
+struct student {
+    char name[50];
+    int rollNo;
+    float midMarks;
+};
+
+struct stack* create_stack(struct stack* stack) {
     struct stack* newStack;
+    newStack = (struct stack*)malloc(sizeof(struct stack));
 
-    newStack=(struct stack*)malloc(sizeof(struct stack));
-
-    if(newStack)
-    {
-        newStack->count=0;
-        newStack->top=NULL;
+    if (newStack) {
+        newStack->count = 0;
+        newStack->top = NULL;
     }
     return newStack;
 }
-bool pushStack(struct stack* newStack,void* data_ins)//pushes/creates a linked list stack.
-{
+
+bool pushStack(struct stack* newStack, void* data_ins) {
     struct node* newNode;
+    newNode = (struct node*)malloc(sizeof(struct node));
 
-    newNode=(struct node*)malloc(sizeof(struct node));
-
-    if(!newNode)
-    {
+    if (!newNode) {
         return false;
-    }else
-    {
-        newNode->data=data_ins;
-        newStack->top=newNode;
-
-        (newStack->count)++;
+    } else {
+        newNode->data = data_ins;
+        newNode->link = newStack->top;
+        newStack->top = newNode;
+        newStack->count++;
     }
+    return true;
 }
-void* popStack(struct stack* newStack)
-{
-    void* data_pop;
 
+void* popStack(struct stack* newStack) {
+    void* data_pop;
     struct node* pNew;
-    if(newStack->count==0)
-    {
-        data_pop=NULL;
-    }else
-    {
-        pNew=newStack->top;
-        data_pop=newStack->top->data;
-        newStack->top=newStack->top->link;
+
+    if (newStack->count == 0) {
+        data_pop = NULL;
+    } else {
+        pNew = newStack->top;
+        data_pop = newStack->top->data;
+        newStack->top = newStack->top->link;
         free(pNew);
-        (newStack->count)--;
+        newStack->count--;
     }
     return data_pop;
 }
-void* topStack(struct stack* newStack)
-{
-    if(newStack->count==0)
-    {
+
+void* topStack(struct stack* newStack) {
+    if (newStack->count == 0) {
         return NULL;
-    }else
-    {
+    } else {
         return newStack->top->data;
     }
 }
-int countStack(struct stack* newStack)
-{
+
+int countStack(struct stack* newStack) {
     return newStack->count;
 }
-bool fullStack(struct stack* newStack)
-{
+
+bool fullStack(struct stack* newStack) {
     struct node* pNew;
-    if(pNew=(struct node*)malloc (sizeof(*(newStack->top))))
-    {
+    if (pNew = (struct node*)malloc(sizeof(*(newStack->top)))) {
         free(pNew);
         return false;
     }
     return true;
 }
-bool emptyStack(struct stack* newStack)
-{
- return (newStack->top==0);
-}
 
-int main()
-{
-    
+bool emptyStack(struct stack* newStack) {
+    return (newStack->top == NULL);
 }
